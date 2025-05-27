65fbac0e20ff8ac74a346e9f148f26bd488ee4c7
nanafranck <nanafranck94@gmail.com>
2025-05-27 22:50:19 +0200

    init project

---
+++ src/main/java/com/school_management/models/User.java
@@ -0,0 +1,9 @@
+package com.school_management.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String nom;
+    private String prenom;
+}
---
+++ src/test/java/com/school_management/SchoolManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package com.school_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
