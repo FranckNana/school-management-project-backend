2eb5e766b5b34ccd668fc85a1535d340412200ff
nanafranck <nanafranck94@gmail.com>
2025-06-02 11:05:15 +0200

    add controller hellowork

---
+++ src/main/java/com/school_management/controllers/Hellowork.java
@@ -0,0 +1,16 @@
+package com.school_management.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Hellowork {
+
+    @GetMapping("/hello")
+    public String hellowork(){
+        return "<h1>Hello World</h1>";
+    }
+}
+
